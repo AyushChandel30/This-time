4e6bcf93f8955ff0faa9efab9ffde812ac54a6c8
AyushChandel30 <ayush.chibu@gmail.com>
2023-11-18 14:44:11 +0530

    commiting

---
+++ src/main/java/how/com/ThisTimeApplication.java
@@ -0,0 +1,13 @@
+package how.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/how/com/ThisTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package how.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
